2e11fad848d273e973d54da38a3529c4866175bf
raidelacruz <ryuxsein@gmail.com>
2026-07-31 17:49:58 -0500

    proyectoCanchita

---
+++ canchitasanmartin/src/main/java/com/proyectocanchita/canchitasanmartin/CanchitasanmartinApplication.java
@@ -0,0 +1,13 @@
+package com.proyectocanchita.canchitasanmartin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanchitasanmartinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanchitasanmartinApplication.class, args);
+	}
+
+}
---
+++ canchitasanmartin/src/test/java/com/proyectocanchita/canchitasanmartin/CanchitasanmartinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectocanchita.canchitasanmartin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanchitasanmartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
