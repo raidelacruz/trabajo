4b58768e2ce922604087beaab42c2e4c24c127db
raidelacruz <ryuxsein@gmail.com>
2026-09-07 12:53:27 -0500

    commit manejo de excepciones parte 2

--- canchitasanmartin/src/main/java/com/proyectocanchita/canchitasanmartin/common/exception/GlobalExceptionHandler.java
+++ canchitasanmartin/src/main/java/com/proyectocanchita/canchitasanmartin/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.proyectocanchita.canchitasanmartin.common.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -42,13 +46,27 @@ public class GlobalExceptionHandler {
         .timestamp(LocalDateTime.now())
         .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
         .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
-        .message(exception.getMessage("Ocurrió un error inesperado en el servidor. Por favor intente más tarde."))
+        .message("Ocurrió un error inesperado en el servidor. Por favor intente más tarde.")
         .path(request.getRequestURI())
         .build();
         return ResponseEntity.status(HttpStatus. INTERNAL_SERVER_ERROR).body(error);    
     }
 
-
-    @ExceptionHandler(MethodArgumentValid.class)
-    public ResponseEntity<ErrorResponseDTO>
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDTO> manejarValidacion(MethodArgumentNotValidException exception, HttpServletRequest request){
+        
+         Map<String,String> errores = new HashMap<>(); 
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            errores.put(fieldError.getField(),fieldError.getDefaultMessage());
+        }
+        ErrorResponseDTO error = ErrorResponseDTO.builder()
+        .timestamp(LocalDateTime.now())
+        .status(HttpStatus.BAD_REQUEST.value())
+        .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+        .message("Error de validación en los campos enviados")
+        .path(request.getRequestURI())
+        .errors(errores)
+        .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 }
